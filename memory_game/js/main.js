0240fdc098f618882aaa91c4cb4e8eb8d205185e
Kayamori Takeru <TAKERU@Kayamori-no-MacBook-Pro.local>
2018-12-11 22:26:50 -0800

    Complete memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,32 +1,62 @@
-console.log("Up and running!");
+
+
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	}, {
+	rank: "queen", 
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	}, {
+	rank: "king", 
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	}, {
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+]; 
 
 
 
+var cardsInPlay = [];
 
-
-var cards = ["queen", "queen", "king", "king"];
-
-var cardsInPlay =[];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
-		alert("Sorry, try again");
+		alert("Sorry, try again.");
 	}
+};
 
-} else {
-	console.log("There's something wrong");
-}
 
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardThree);
+var flipcard = function() {
+	
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+var createBoard = function () {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipcard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+createBoard();
+
